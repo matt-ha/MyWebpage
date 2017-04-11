b58f1d4f4f6b11b55f677d245f06850976b0ece2
matthew ha <matthew ha>
2017-04-11 19:44:41 -0400

    add error reporting

--- action_page.php
+++ action_page.php
@@ -1,5 +1,8 @@
 <?php
 require_once '\vendor\autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $Name = $_GET['Name'];
 $Email = $_GET['Email'];

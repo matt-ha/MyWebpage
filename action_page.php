3506aba2684fddf7c78b667ec1be609789f6a59a
matthew ha <matthew ha>
2017-04-11 20:19:21 -0400

    remove error logging

--- action_page.php
+++ action_page.php
@@ -1,8 +1,5 @@
 <?php
 require_once getcwd() . '/vendor/autoload.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 $Name = $_GET['Name'];
 $Email = $_GET['Email'];

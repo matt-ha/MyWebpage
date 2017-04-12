d980575935ad79921df48a121d924f485093bfb2
matthew ha <matthew ha>
2017-04-11 20:04:15 -0400

    use absolute path for autoload.php

--- action_page.php
+++ action_page.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/vendor/autoload.php';
+require_once getcwd() . '\vendor\autoload.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);

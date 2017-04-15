d04783ed838d7e535e8d584ab61cb93f9dada786
matthew ha <matthew ha>
2017-04-15 04:50:56 -0400

    envir keys

--- action_page.php
+++ action_page.php
@@ -1,21 +1,21 @@
 <?php
 require_once getcwd() . '/vendor/autoload.php';
-//$ini = parse_ini_file('/vendor/test/config.ini');
+$ini = parse_ini_file('/vendor/test/config.ini');
 
 $Name = $_GET['Name'];
 $Email = $_GET['Email'];
 $Message = $_GET['Message'];
 
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-  ->setUsername($ini['app_email'])
-  ->setPassword($ini['app_pass']);
+  ->setUsername(getenv('app_email'))
+  ->setPassword(getenv('app_pass'));
 
 
 $mailer = Swift_Mailer::newInstance($transport);
 
 $message = Swift_Message::newInstance($Name)
   ->setFrom(array('myemail@hotmail.com' => $Email))
-  ->setTo(array($ini['app_email2']))
+  ->setTo(array(getenv('app_email')))
   ->setBody("From: <$Email> \n\n" . $Message);
 
 $result = $mailer->send($message);

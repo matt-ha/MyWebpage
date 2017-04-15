c913e773b840593fa70b83f352b2249bf718f19f
matthew ha <matthew ha>
2017-04-15 03:42:17 -0400

    new email added

--- action_page.php
+++ action_page.php
@@ -1,20 +1,21 @@
 <?php
 require_once getcwd() . '/vendor/autoload.php';
+$ini = parse_ini_file('/test/config.ini');
 
 $Name = $_GET['Name'];
 $Email = $_GET['Email'];
 $Message = $_GET['Message'];
 
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-  ->setUsername('ha.ha.ha.matt@gmail.com')
-  ->setPassword('Qpwoeiru10293847');
+  ->setUsername($ini['app_email'])
+  ->setPassword($ini['app_pass']);
 
 
 $mailer = Swift_Mailer::newInstance($transport);
 
 $message = Swift_Message::newInstance($Name)
-  ->setFrom(array('g2peee@hotmail.com' => $Email))
-  ->setTo(array('mattha0929@gmail.com'))
+  ->setFrom(array('myemail@hotmail.com' => $Email))
+  ->setTo(array($ini['app_email2']))
   ->setBody("From: <$Email> \n\n" . $Message);
 
 $result = $mailer->send($message);
